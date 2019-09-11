7798ed7dd1db7c7c2b847326a0c5099962c9a917
br1-gh <viceofchaos@gmail.com>
2019-09-11 21:11:20 +0200

    Complete Assignment. Correct tests.

--- src/main/java/guru/springframework/spring5webfluxrest/controllers/VendorController.java
+++ src/main/java/guru/springframework/spring5webfluxrest/controllers/VendorController.java
@@ -3,9 +3,9 @@ package guru.springframework.spring5webfluxrest.controllers;
 
 import guru.springframework.spring5webfluxrest.domain.Vendor;
 import guru.springframework.spring5webfluxrest.repositories.VendorRepository;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.reactivestreams.Publisher;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
@@ -22,12 +22,18 @@ public class VendorController {
     }
 
     @GetMapping("/api/v1/vendors")
-    Flux<Vendor> list(){
+    public Flux<Vendor> list(){
         return vendorRepository.findAll();
     }
 
     @GetMapping("api/v1/vendors/{id}")
-    Mono<Vendor> getById(@PathVariable String id){
+    public Mono<Vendor> getById(@PathVariable String id){
         return vendorRepository.findById(id);
+    }
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping("api/v1/vendors")
+    public Mono<Void> createVendor(@RequestBody Publisher<Vendor> vendor) {
+        return vendorRepository.saveAll(vendor).then();
     }
 }
